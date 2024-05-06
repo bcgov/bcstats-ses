aef3aff81c29f4ca79aa836256ee1b03269d4a44
Duan <Jon.Duan@gov.bc.ca>
2024-05-06 09:12:01 -0700

    add another option for census data using API.

--- src/01_clean_api.r
+++ src/01_clean_api.r
@@ -40,3 +40,45 @@ van_da <- get_census(dataset='CA21', regions=list(CMA="59933"),
 # Aggregation: indicates how the variable should be aggregated with others, whether it is additive or if it is an average of another variable
 # Description: a rough description of a variable based on its hierarchical structure. This is constructed by cancensus by recursively traversing the labels for every variable’s hierarchy, and facilitates searching for specific variables using key terms.
 vector_list_21 = list_census_vectors("CA21")
+
+
+
+########################################################################################################
+#  Community Well-Being Index
+# install.packages("remotes")
+# remotes::install_github("vlucet/rgovcan")
+# https://open-canada.github.io/r4gc/canada-related-open-source-r-codes-and-packages.html
+########################################################################################################
+
+library("rgovcan")
+# start with a package id corresponding to an actual record and retrieve a ckan_package.
+# https://open.canada.ca/data/en/dataset/56578f58-a775-44ea-9cc5-9bf7c78410e6
+id <- "56578f58-a775-44ea-9cc5-9bf7c78410e6" # Package ID
+id_search <- govcan_get_record(record_id = id)
+id_search # outputs a `ckan_package`
+
+# Once the packages have been retrieved from the portal, you can use govcan_get_resources on those results to display the ckan_resources contained in the packages (a “resource” is any dataset attached to a given record). This outputs a ckan_resource_stack when called on a unique package.
+id_resources <- govcan_get_resources(id_search)
+id_resources # outputs a `resource_stack`
+# download the resources with govcan_dl_resources(). These can either be stored to a certain directory or load into session (* this option might fail due to current issues with ckanr::ckan_fetch).
+
+path <- "G:\\Operations\\Data Science and Analytics\\2024 SES Index\\data\\CWBI"
+dir.create(path, recursive = TRUE)
+
+govcan_dl_resources(id_resources, path = path, included = c("CSV"))
+
+for (i in id_resources){
+  print(i)
+  str(i)
+  tryCatch(
+  {govcan_dl_resources(i, path = path, included = c("CSV"))},
+  
+  error = function(e){
+    message("data is not available")
+  }
+  
+  )
+}
+
+
+
