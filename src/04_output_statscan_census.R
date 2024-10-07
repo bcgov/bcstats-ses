eeb4031fc97af359d216f1515b01e627772986c1
Duan <Jon.Duan@gov.bc.ca>
2024-10-07 14:45:33 -0700

    clean the 04_output_statscan-census.R, and move background into a txt file in the census folder.

--- src/04_output_statscan_census.R
+++ src/04_output_statscan_census.R
@@ -1,152 +1,19 @@
 # this file is used for downloading data from StatsCan and other source. 
 # library("remotes")
 # install_github("bcgov/safepaths")
-pacman::p_load(cancensus,geojsonsf, tidyverse,config,bcmaps, bcdata, janitor,cansim,safepaths, arrow, duckdb)
+pacman::p_load(cancensus,geojsonsf, sf, tidyverse,config,bcmaps, bcdata, janitor,cansim,safepaths, arrow, duckdb)
 
-######################################################################################
-# 
-# 
-# Census Divisions (CD) are the principal geographic areas for the country-wide census and are created by Statistics
-# Canada. There are currently 28 CDs in B.C. (2011 Census). Census Division boundaries in Canada follow counties,
-# regional districts, regional municipalities, and five other types of geographic areas made up of groups of Census
-# Subdivisions (CSDs). In British Columbia the CD is comprised of one unorganized region (Stikine) and 27 Regional
-# Districts which have local government functions. CD boundaries follow Regional Districts so they are affected by
-# Regional District boundary changes, such as extensions, reductions, name changes, and newly created Regional
-# Districts.
-# Census Subdivisions (CSD) aggregate to Census Divisions (CDs), which in turn aggregate to a province or territory
-# (the province code for British Columbia is 59). This relationship is reflected in the 7-digit Standard Geographical
-# Classification code (SGC). The TMF SGC is slightly different from the Statistics Canada SGC due to the exclusion of the
-# province code. Therefor the TMF value for Census Division and Subdivision is referred to as the two digit CD code and
-# a three digit CSD code.
-# it is important to note that not all of the 837 CSD’s in the province will be represented on the TMF(Translation Master File which is the BC Stats official classification for geographic areas), as the appearance of
-# a CSD on the TMF is dependent upon whether or not there is a postal code geocoded to that area. A large number of
-# the RDEA’s and the vast majority of IRs are not represented on the TMF
 
-# Census Subdivision (CSD) CSDs are municipalities (as determined by provincial legislation) or areas treated as municipal equivalents 
-# 
-######################################################################################
 
 
 ######################################################################################
 # Census data
 ######################################################################################
+# see census/background.txt to get informaiton for installation and background.
 
-
-# the location is saved in config.yaml file. 
-
-
-# set up CENSUSMAPPER API 
-
-# set_cancensus_api_key(config::get("CENSUSMAPPER_API_KEY"), install = TRUE)
-
-# In order to speed up performance, reduce API quota usage, and reduce unnecessary network calls, please set up a persistent cache directory via `set_cancensus_cache_path('<local cache path>', install = TRUE)`.
-# This will add your cache directory as environment variable to your .Renviron to be used across sessions and projects.
-# set_cancensus_cache_path(use_network_path("data/census_cache"), install = TRUE)
-# or
-# options(cancensus.api_key = "your_api_key")
-# options(cancensus.cache_path = "custom cache path")
-
-# options(cancensus.api_key = config::get("CENSUSMAPPER_API_KEY"))
-# options(cancensus.cache_path = use_network_path("data/census_cache"))
-
-# getOption("cancensus.cache_path")
-# Your cache path has been stored in your .Renviron and can be accessed by 
-Sys.getenv("CM_CACHE_PATH")
-
-
-library(sf)
-
-list_census_datasets()
-
-
-# This test task has been done, so skip it. We can delete this part after the review
-# retrieve sf dataframe
-
-# van <- get_census(dataset='CA21', regions=list(CMA="59933"),
-#                       vectors=c("median_hh_income"="v_CA21_906"), level='CSD', quiet = TRUE, 
-#                       geo_format = 'sf', labels = 'short')
-# 
-# # DA	Dissemination Area	56589
-# # DB	Dissemination Block (2001-2016)	489676
-# 
-# van_db <- get_census(dataset='CA21', regions=list(CMA="59933"),
-#                   vectors=c("median_hh_income"="v_CA21_906"), level='DB', quiet = TRUE, 
-#                   geo_format = NA, labels = 'short')
-
-
-# van_da <- get_census(dataset='CA21', regions=list(CMA="59933"),
-#                      vectors=c("median_hh_income"="v_CA21_906"), level='DA', quiet = TRUE, 
-#                      geo_format = NA, labels = 'short')
-
-# view all available Census variables for a given dataset.
-# Vector: short variable code
-# Type: variables are provided as aggregates of female responses, male responses, or total (male+female) responses
-# Label: detailed variable name
-# Units: provides information about whether the variable represents a count integer, a ratio, a percentage, or a currency figure
-# Parent_vector: shows the immediate hierarchical parent category for that variable, where appropriate
-# Aggregation: indicates how the variable should be aggregated with others, whether it is additive or if it is an average of another variable
-# Description: a rough description of a variable based on its hierarchical structure. This is constructed by cancensus by recursively traversing the labels for every variable’s hierarchy, and facilitates searching for specific variables using key terms.
-vector_list_21  <-  list_census_vectors("CA21")
-# A tibble: 7,709 × 7
-
-
-
-# Selected variable list from census 2021
-# "1", # pop
-# "6", # pop_dens
-# '8', #
-# '14','15', # age 15 24, 
-# '35','37',  # dep_rat
-# "41",'42', # det_homes
-# "50" , # Total - Private households by household size - 100% data
-# "57", # Average household size
-# "78", "86", #sing_par
-# '115', # med_at_inc
-# "151", # gov_trans
-# 
-# "244", # Median after-tax income of household in 2020 ($)
-# '345', # lim_at
-# 
-# "381", # Gini index on adjusted household after-tax income
-# "382", # P90/P10 ratio on adjusted household after-tax income
-# "1402", "1403", # pc_indig
-# "1414", "1416", # pct_rent
-# 
-# "1470", # Total - Households 'spending 30% or more of income on shelter costs' or 'not suitable' or 'major repairs needed'
-# "1472", # Not suitable only: too crowded
-# "1473", # Major repairs needed only
-# "1476", #  'Not suitable' and 'major repairs needed'
-# "1477", # 'Spending 30% or more of income on shelter costs' and 'not suitable' and 'major repairs needed'
-# "1537", # pct_npr "1"
-# "1683",  "1684", # pct_vismin
-# "1995", "1996",  #pct_nograd
-# "2223", "2224", # labor_part_rate
-# "2226", #labor_unemp_rate "2224"
-# "2246", "2249" #pct_mgmt_occ
-
-# v_CA21_1143
-# Total
-# P90/P10 ratio on adjusted household after-tax income
-# Income; Inequality measures for the population in private households; P90/P10 ratio on adjusted household after-tax income
-
-
-# get census data vectors for labor force involvement rates
-# em_labels <- c("Number of employment income recipients aged 15 years and over in private households in 2020", "Median employment income in 2020 among recipients", "Number of employment insurance benefits recipients aged 15 years and over in private households in 2020")
-# em_vectors <- 
-#   find_census_vectors("employment", dataset= "CA21", type = "total", query_type = "semantic") %>% 
-#   filter(str_detect(label, 
-#                     pattern = paste(em_labels, collapse = "|")
-#                      ))
-#   
-#   # union(search_census_vectors("labor", "CA21"))
-# lf_labels <- c("Total - Population aged 15 years and over by labour force status", )
-# lf_vectors <-  find_census_vectors("labor force", dataset= "CA21", type = "total", query_type = "semantic") 
-# 
-# income_vector = find_census_vectors('after tax income', dataset = 'CA21', type = 'total', query_type = 'semantic')
-
-# The better way may be to get the vecor list first, and then retrieve the meta data using cancensus function, at the end, create variable name in a csv file and use that file to create this named vector list. 
-# We want to keep the vector which is the key to link cancensus table. 
-# maybe we only get a long form table, and pivot to a wide table later. 
+# if you get the cache for cancensus package working, set use_cache flag as TRUE, then it will pull data from cache.
+# Otherwise, keep the use_cache flag as FALSE, then it will download the data from statscan. It may take a few minutes. 
+use_cache = TRUE
 
 
 # Put all the variable strings together: 
@@ -455,6 +322,7 @@ bc_da <- get_census(dataset='CA21',
                     level='DA', 
                     quiet = TRUE, 
                     geo_format = NA,
+                    use_cache = use_cache,
                     labels = 'short')
 
 
