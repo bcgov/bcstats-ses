b2985e7682b51eee887a1ffdbb1c46fc2747754f
thister <tim.hister@gov.bc.ca>
2024-11-05 13:39:19 -0800

    did the import part - now trying to figure out the dictionary

---
+++ src/05_output_LFS.R
@@ -0,0 +1,186 @@
+# this file is used for outputting data for importing to DIP
+
+pacman::p_load(cancensus, geojsonsf, tidyverse, config, bcmaps, bcdata, janitor, cansim, safepaths, arrow, duckdb, datadictionary)
+
+LFS_file = use_network_path("data/StatsCanLFS/1410045701_databaseLoadingData.csv")
+
+LFS = readr::read_csv(TMF_file)
+
+# only BC
+LFS = LFS |>
+  filter(str_detect(GEO, "British Columbia")) |>
+  mutate(GEO = str_replace(GEO, ", British Columbia", ""))
+
+# clean the names. We prefer all uppercase
+LFS = LFS |> 
+  janitor::clean_names(case = "screaming_snake" ) 
+
+# REF_DATE is monthly data so we can use zoo's yearmon()
+LFS = LFS |>
+  mutate(REF_DATE = zoo::as.yearmon(LFS$REF_DATE))
+
+# DGUID, SYMBOL, and TERMINATED are only NAs; dump 'em
+LFS = LFS |> select(where(~ !all(is.na(.))))
+
+# SCALAR_FACTOR and SCALAR_ID have only one unique value, "units" and 0, respectively. Dump these columns.
+LFS = LFS |> 
+  select(
+    LFS |> 
+      summarise(across(everything(), n_distinct)) |>
+      pivot_longer(cols = everything()) |>
+      filter(value > 1) |>
+      pull(name)
+  )
+
+LFS$GEO |> unique() |> sort()
+
+# the geography type (CA = "census agglomeration", CMA = "census metropolitan area" and SLA = "self-contained labour area") is printed in square brackets at the end of GEO. This is not good. Let's split this type out in a new column called GEO_TYPE and remove it from GEO.
+LFS = LFS |>
+  mutate(GEO_TYPE = str_extract(GEO, "\\[(.+)\\]", group=1), .after='GEO') |>
+  mutate(GEO = str_remove(GEO, " \\[.+\\]"))
+
+# UOM_ID and DECIMALS are integers, not doubles
+LFS = LFS |> mutate(across(c(UOM_ID, DECIMALS), as.integer))
+
+# Convert cols GEO_TYPE, LABOUR_FORCE_CHARACTERISTICS, STATISTICS, UOM and STATUS to factors
+LFS = LFS |> mutate(across(c(GEO_TYPE, LABOUR_FORCE_CHARACTERISTICS, STATISTICS, UOM, STATUS), as.factor))
+
+LFS |> readr::write_csv(here::here("out", "LFS_DIP.csv"))
+
+
+#################################################################################################
+# Data dictionary
+#################################################################################################
+LFS_dict = create_dictionary(LFS)
+
+View(LFS_dict)
+
+# f2 <- "https://www2.gov.bc.ca/assets/gov/health/forms/5512datadictionary.pdf"
+# a better data dictionary in Geocoding Self-Service (GCS) User Guide Prepared by BC Stats March 2020, not online, provided by Brett.
+# f3 <-  use_network_path("docs/GCS_User_Guide.pdf")
+
+# it does not work well using pdf tools in R. so we had to manually make a csv file for it.
+
+# manually create a item field in this detail dataframe to join the TMF_dict dataframe
+# it is too hard to do it mannually. Chatgpt did it.
+
+library(readr)
+TMF_dict_detail <- read_csv( use_network_path("docs/TMF_data_dict.csv"))
+View(TMF_dict_detail)
+
+
+# TMF_dict_detail %>% pull(`Field Name`)
+# 
+# # Your vector of strings
+# strings <- TMF_dict_detail %>% pull(`Field Name`)
+# 
+# # Create the mapping
+# mapping <- setNames(strings, strings)
+# 
+# # Custom print function
+# formatted_output <- paste0('"', names(mapping), '" = "', mapping, '"', collapse = ", ")
+# 
+# # Print the formatted string
+# cat(formatted_output)
+
+# then I can copy the results to our new switch function
+# maybe better just do it in CSV file.
+# or I should use those as labels in create_dictionary function. liek c(POSTAL_CODE = "Postal code")
+# finally use chatgpt to translate the switch function to a case_when
+create_item = function(x) {
+  # Using case_when to replace the switch statement
+  y <- case_when(
+    x == "Postal code" ~ "POSTALCODE",
+    x == "Birth Date" ~ "BIRTH_DATE",
+    x == "Retired Date" ~ "RET_DATE",
+    x == "Latitude" ~ "LATITUDE",
+    x == "Longitude" ~ "LONGITUDE",
+    x == "Census Division" ~ "CD",
+    x == "Census Subdivision" ~ "CSD",
+    x == "Census Subdivision Name" ~ "MUN_NAME",
+    x == "Census Metropolitan Area or Census Agglomeration Area" ~ "CMACA",
+    x == "Dissemination Area (DA)" ~ "DA",
+    x == "Census Tract (CT)" ~ "CT",
+    x == "Dissemination Block (DB)" ~ "DB",
+    x == "Designated Place Name (DPL)" ~ "DPL",
+    x == "Population Centre (POPCTR)" ~ "POPCTR",
+    x == "Development Region (DR)" ~ "DR",
+    x == "Health Authority (HA)" ~ "HA",
+    x == "Health Service Delivery Area (HSDA)" ~ "HSDA",
+    x == "Local Health Area (LHA)" ~ "LHA",
+    x == "Community Health Service Area (CHSA)" ~ "CHSA",
+    x == "Pre-2018 Local Health Area" ~ "LHA_PRE_2018",
+    x == "Micro Health Area" ~ "MHA",
+    x == "Ministry of Children & Families Region (MCFD)" ~ "MCFD",
+    x == "Ministry of Children & Families Service Delivery Area (MCFD_SDA)" ~ "MCFD_SDA",
+    x == "Ministry of Children & Families Local Service Area (MCFD_LSA)" ~ "MCFD_LSA",
+    x == "College Region" ~ "CR",
+    x == "School District (SD)" ~ "SD",
+    x == "School District Trustee Electoral Area (TEA)" ~ "TEA",
+    x == "Provincial Electoral District (PED)" ~ "PED",
+    x == "Federal Electoral District (FED)" ~ "FED",
+    x == "Police Services Respondent Area (RESP)" ~ "RESP",
+    x == "Tourism Region" ~ "TOURISM",
+    x == "Games Zone" ~ "GZ",
+    x == "Community Name" ~ "COMM_NAME",
+    x == "Modified Census Subdivision Name" ~ "Modified_MUN_NAME",
+    x == "Modified Full Census Subdivision" ~ "CDCSD",
+    TRUE ~ NA_character_  # Default case if no match
+  )
+  
+  return(y)
+}
+
+TMF_dict_detail <- TMF_dict_detail %>%
+  mutate(item_short = create_item(`Field Name`))
+
+# for the datadictionary created from datadictionary function, we also need to create a shorten item name since some items have year as sufix such as CD_2021. 
+TMF_dict <- TMF_dict %>% 
+  mutate( item_short = str_remove(item, "_\\d{4}")) 
+
+
+# TMF_dict's summary for numerica number does not mean a lot since it is a fact table, and all dimension tables are in another lookup.xlsx excel file. 
+# so we could simplify the TMF_dict
+# TMF_dict_simple <- TMF_dict %>% 
+#   group_by(item) %>% 
+# slice_head(n = 1)
+# filter(row_number() == 1)
+
+# or it does not matter, we can add value label later using our dimension table
+TMF_dict <- TMF_dict %>% 
+  left_join(TMF_dict_detail, join_by( item_short))
+
+
+TMF_dict %>% readr::write_csv(here::here("out", "Translation_Master_File_Dict_DIP.csv"))
+
+
+#################################################################################################
+# Now we deal with the dimension table aka lookup tables
+#################################################################################################
+
+# Load necessary libraries
+library(readxl)
+
+# Path to the Excel file
+file_path <- use_network_path("data/GCS_Lookup_Table.xlsx")
+
+# Specify the prefix for the CSV files
+prefix <- "Translation_Master_File_Lookup_"
+
+# Get the sheet names
+sheet_names <- excel_sheets(file_path)
+
+# Loop through each sheet and save as CSV with a prefix
+for (sheet in sheet_names) {
+  # Read the sheet
+  data <- read_excel(file_path, sheet = sheet)
+  
+  # Create the CSV file name with the prefix
+  csv_file_name <- here::here("out", paste0(prefix, sheet, ".csv"))
+  
+  # Save the sheet as a CSV file
+  write.csv(data, csv_file_name, row.names = FALSE)
+  
+  # Print message for confirmation
+  message(paste("Saved:", csv_file_name))
+}
