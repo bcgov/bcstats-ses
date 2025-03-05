87eca0378d970c7c80504d9fd02bd31ab1f58396
Duan <Jon.Duan@gov.bc.ca>
2025-03-04 17:09:44 -0800

    refactor the remoteness code and move functions to utils.r

--- src/09_output_remoteness.R
+++ src/09_output_remoteness.R
@@ -12,27 +12,21 @@
 
 ####################################################################
 # This script reads distance and drive time data from home address to nearest facilities(Service BC office, school, and hospital) data from geo-location service team in BC Data Service. 
-# This data is link to the shape file for Dessimination Block/Dessimination Area to get a DB/DA id for the home, so we can calculate the DB/DA level average distance and drive time. 
-# Based on the DB/DA level data, we can calculate the CSD level average distance and drive time.
-# Brian updated the data with DA id and randomly sampled addresses within DA strata in 2025-02-11. 
-# We have two sets of data, one is the original data which do not have DA id and randomly sampled within locality id strata, and the other is the updated data.
-# Previously, we added DA id into the original data using st_join function, and now we will combine the original data with the updated data.
-# Therefore, we need to deal with potential duplicated row issues. 
-# TODO: previous, when we drop the geometry from a sf object, we lost the coordinates columns. 
+# Geodata team has updated the data with DA id and randomly sampled addresses within DA strata in 2025-02-11. 
 # We need to keep the coordinates columns when we drop the geometry column, or we need to add the coordinates columns back after we drop the geometry column from original data.
 ###################################################################
-
 
 # Load necessary libraries
 library(sf)
 library(ggplot2)
 library(dplyr)
-pacman::p_load(cancensus,geojsonsf, tidyverse,config,bcmaps, bcdata, janitor,cansim,safepaths, arrow, duckdb, cancensus)
+pacman::p_load(cancensus, geojsonsf, tidyverse, config, bcmaps, bcdata, janitor, cansim, safepaths, arrow, duckdb, cancensus)
 library(jsonlite)
 library(cowplot) # For aligning multiple plots
-library(patchwork )
+library(patchwork)
 # Load the rlang package for the bang-bang operator
 library(rlang)
+source("utils.R") # get the functions for plotting maps
 
 ###################################################################
 # # boundary files
@@ -44,20 +38,15 @@ library(rlang)
 
 ###################################################################
 
-
 # To retrieve an option
-
 getOption('timeout')
 # [1] 60
-
 # To set an option
-
 options(timeout=600)
 
 ###########################################################################################
 # DA shp file
 # since geodata team added DA id in the dataset, we can skip this step now. Prevously, we have to calculate the DA id using st_join function.
-# we can jump to line 251
 ###########################################################################################
 
 # grab the file from "Statistics Canada", only run once.
@@ -70,8 +59,6 @@ file_path <- use_network_path("2024 SES Index/data/raw_data/remoteness/lda_000a2
 # 
 # # Load the dissemination area shapefile
 da_shapefile <- st_read(file_path)
-# 
-# 
 da_shapefile <- da_shapefile %>%
     filter(PRUID == '59') %>%
     st_transform(crs=3005)
@@ -162,8 +149,6 @@ address_sf_with_da <- new_da_servicebc_df2 %>%
   st_as_sf(coords = c("SITE_ALBERS_X", "SITE_ALBERS_Y"), crs = 3005)
 
 
-
-
 # What is the nan in TAG?
 new_da_servicebc_df2 %>% 
   bind_rows(new_da_hospital_df) %>% 
@@ -178,6 +163,10 @@ new_da_servicebc_df2 %>%
   janitor::clean_names(case = "screaming_snake") %>% 
   filter(TAG == "nan")
 
+
+# Save the data to csv file
+# This file will be used in the next step for calculating the drive time and distance to the nearest facility
+# for each address.
 new_da_servicebc_df2 %>% 
   bind_rows(new_da_hospital_df) %>% 
   bind_rows(new_da_school_df) %>% 
@@ -185,6 +174,7 @@ new_da_servicebc_df2 %>%
   rename(ADDRESS_ALBERS_X = SITE_ALBERS_X,
          ADDRESS_ALBERS_Y = SITE_ALBERS_Y)  %>% 
   write_csv(use_network_path("2024 SES Index/data/raw_data/remoteness/bc_address_with_da.csv"))
+
 
 ###########################################################################################
 #   create a DA level summary. 
@@ -361,12 +351,6 @@ csd_avg_address_dist_sf = csd_shapefile %>%
   left_join( CSD_REMOTENESS_BY_SERVICE ,
              join_by(CSDUID ) )
   
-  
-  test3 = csd_avg_address_dist_sf %>% 
-    filter(MUN_NAME_2021 %in% c("Vancouver", "Smithers")) %>% 
-    filter(!is.na(AVG_DRV_DIST))
-  
-
 csd_avg_address_dist_sf_servicebc_p <- ggplot(data = csd_avg_address_dist_sf %>% filter(TAG == 'servicebc')
        ) +
   geom_sf(aes(fill = AVG_DRV_DIST), color = "gray") +
@@ -388,238 +372,13 @@ ggsave(use_network_path("2024 SES Index/data/output/remoteness/csd_avg_address_d
 
 
 ################################################################
-# interesting these results weren't what I was as expecting for the least remote. assuming longest drive time = most remote, then Smithers is less remote than Vancouver by this definition.
+# interesting these results weren't what I was as expecting for the least remote. 
+# assuming longest drive time = most remote, then Smithers is less remote than Vancouver by this definition.
 # we can plot them in the map
 ################################################################
 
-CSD_DA_avg_dist_drvtime_by_service_sf <- address_avg_dist_da_shapefile %>% 
-  left_join(CSD_DA_list, by = join_by("DAID" == "DA_NUM") )
-
-  
-CSD = "Vancouver"
-facility = "servicebc"
-test4 <-  facility_sf %>% 
-  filter(TAG == service) %>% 
-  filter(MUN_NAME_2021 == CSD) 
-
-
-########## 
-#  A map plotting function for a CSD region
-# plot DAs in a CSD region with the average distance to the nearest servicebc/schools/hospitals
-##########  
-# add a bright red dots for the facilities. 
-# 
-# To ensure that the same data values map to the same colors in both maps, 
-# use identical fill scales (same limits, breaks, and palette) in both ggplot objects. 
-# Then, when assemble them with patchwork, set the guides to "collect" so that a single shared legend is used. 
-
-min_val<- 0 #range(CSD_DA_avg_dist_drvtime_by_service_sf$AVG_DRV_DIST, na.rm = T)[1]
-max_val <- 12 #range(CSD_DA_avg_dist_drvtime_by_service_sf$AVG_DRV_DIST, na.rm = T)[2] 
-step  <- 1.5
-
-#' Plot a map for a given Census Subdivision (CSD) showing the average distance to a specific facility (e.g., servicebc, schools, hospitals).
-#' 
-#' This function generates a map for a specified Census Subdivision (CSD) that displays the average distance to a given facility (e.g., servicebc, schools, hospitals). The map includes the CSD boundaries, with each area colored based on the average distance to the service. Additionally, the locations of the facilityfacilities are marked with red dots. The map can optionally include labels showing the average distance values.
-#' 
-#' @param CSD Character string specifying the name of the Census Subdivision (CSD) to plot.
-#' @param facility Character string specifying the type of facility to plot (e.g., "servicebc", "schools", "hospitals").
-#' @param fill_var Character string specifying the variable name in the data frame that contains the average distance values.
-#' @param fill_var_name Character string specifying the name of the variable to use in the legend and labels.
-#' @param csd_sf Simple Features object containing the CSD boundaries and average distance data.
-#' @param facility_sf Simple Features object containing the locations of the service facilities.
-#' @param min_val Numeric value specifying the minimum value for the fill scale.
-#' @param max_val Numeric value specifying the maximum value for the fill scale.
-#' @param step Numeric value specifying the step size for the fill scale breaks.
-#' @param label_var Logical value indicating whether to include labels showing the average distance values. Default is FALSE.
-#' 
-#' @return A ggplot object representing the map.
-#' 
-#' @examples
-#' plot_a <- plot_csd_facility_map_fn(CSD = "Vancouver", 
-#'                                    facility = "servicebc", 
-#'                                    fill_var = "AVG_DRV_DIST", 
-#'                                    fill_var_name = "Average distance",
-#'                                    csd_sf = CSD_DA_avg_dist_drvtime_by_service_sf, 
-#'                                    facility_sf = facility_sf,  
-#'                                    min_val = 0, max_val = 12, step = 1.5,
-#'                                    label_var = FALSE)
-#' print(plot_a)
-plot_csd_facility_map_fn <- function(CSD, facility, fill_var,fill_var_name, csd_sf, facility_sf, min_val, max_val, step,  label_var = FALSE){
-  
-  csd_sf <- csd_sf %>% 
-    filter(TAG == facility) %>% 
-    filter(MUN_NAME_2021 == CSD)
-  
-  facility_sf <- facility_sf %>% 
-    filter(TAG == facility) %>% 
-    filter(MUN_NAME_2021 == CSD) 
-  
-  P <- csd_sf %>% 
-    ggplot() +
-    # geom_sf(aes_string(fill = fill_var), color = "white")+ # or use 
-    geom_sf(aes(fill = !!sym(fill_var)))+
-    scale_fill_viridis_c(limits = c(min_val, max_val), breaks = seq(min_val, max_val, by = step))
-  
-  if (label_var) {
-    P <- P + geom_sf_label(aes(label = format(!!sym(fill_var), digits =2)), colour = "black")
-  }
-  
-  P <- P +
-    labs(
-      subtitle = glue::glue("{fill_var_name} to the nearest {facility} in {CSD}"),
-      x = "Longitude",
-      y = "Latitude"
-    ) + 
-    my_map_theme+ 
-    guides(fill=guide_legend(title=glue::glue("Avg. {stringr::str_to_title(fill_var_name)} to the {facility}")))
-  
-  P <- P + geom_point(
-    data = facility_sf,aes(geometry = geometry),
-    shape = 23,  
-    color = "red",
-    fill = "red",
-    size = 5,
-    alpha = 0.5,
-    stat = "sf_coordinates"
-  ) 
-  
-  return(P)
-}
-
-plot_a = plot_csd_facility_map_fn(CSD = "Vancouver", 
-                     facility = "servicebc", 
-                     fill_var = "AVG_DRV_DIST", fill_var_name = "Average distance",
-                     csd_sf =CSD_DA_avg_dist_drvtime_by_service_sf, facility_sf ,  
-                     min_val, max_val, step,
-                     label_var = FALSE )
-
-print(plot_a)
-plot_b = plot_csd_facility_map_fn(CSD = "Smithers",
-                     facility = "servicebc", 
-                     fill_var = "AVG_DRV_DIST", fill_var_name = "Average distance",
-                     csd_sf =CSD_DA_avg_dist_drvtime_by_service_sf, facility_sf ,  
-                     min_val, max_val, step,
-                     label_var = T )
-
-print(plot_b)
-
-
-
-# Combine the two plots using patchwork and add a shared title
-combined_plot <- (plot_a + plot_b +
-                    plot_layout(ncol = 2, guides = "collect") &
-                    theme(legend.position = "bottom")) +
-  plot_annotation(
-    title = "Remoteness measurement: Vancouver vs Smithers",
-    theme = theme(
-      plot.title = element_text(size = 16, face = "bold", hjust = 0.5)
-    )
-  )
-
-# Display the combined plot
-print(combined_plot)
-
-# Save the combined plot
-ggsave(use_network_path("2024 SES Index/data/output/remoteness/two_csd_avg_address_dist_servicebc_sf_by_da.png"),
-       plot = combined_plot,
-       width = 14, height = 7, dpi = 300)
-
 
 ################################################################
-# create a function for the map plot
-#' Compare Two CSDs in a Map
-#'
-#' This function compares two Census Subdivisions (CSDs) by plotting their remoteness measurement on a map. It generates two separate plots for each CSD, displaying the average distance to a specified service (e.g., Service BC office), and combines them into a single plot with a shared title.
-#'
-#' @param CSD1 The name of the first Census Subdivision to compare.
-#' @param CSD2 The name of the second Census Subdivision to compare.
-#' @param facility The name of the facility to measure remoteness from (e.g., "servicebc").
-#' @param fill_var The variable name in the csd_sf data frame that represents the remoteness measurement (e.g., "AVG_DRV_DIST").
-#' @param fill_var_name A descriptive name for the fill_var variable to use in the plot legend.
-#' @param csd_sf A simple features data frame containing the Census Subdivision boundaries and remoteness measurement data.
-#' @param facility_sf A simple features data frame containing the facility locations.
-#' @param label_var1 A logical indicating whether to label the first CSD plot with facility names.
-#' @param label_var2 A logical indicating whether to label the second CSD plot with facility names.
-#'
-#' @return This function returns a combined plot of the two CSDs, displaying their remoteness measurement, and saves the plot to a file.
-#' @export
-compare_two_csd_in_map <- function(CSD1 = "Vancouver",
-                                   CSD2 = "Smithers",
-                                   facility = "servicebc",
-                                   fill_var = "AVG_DRV_DIST",
-                                   fill_var_name = "Average distance",
-                                   csd_sf = CSD_DA_avg_dist_drvtime_by_service_sf,
-                                   facility_sf = facility_sf ,
-                                   label_var1 = F ,
-                                   label_var2 = T) {
-  color_range  <- csd_sf %>% 
-    filter(MUN_NAME_2021 %in% c(CSD1, CSD2)) %>% 
-    filter(!is.na(!!sym(fill_var))) %>% 
-    filter(TAG %in% c(service)) %>% 
-    pull(!!sym(fill_var)) %>% 
-    range()
-  
-  min_val<- floor(color_range[1]) #
-  
-  max_val <- ceiling(color_range[2]) #
-  step  <- (max_val - min_val)/(12-1)
-  
-  plot_a = plot_csd_facility_map_fn(
-    CSD = CSD1,
-    facility ,
-    fill_var ,
-    fill_var_name ,
-    csd_sf ,
-    facility_sf  ,
-    min_val, max_val, step,
-    label_var = FALSE
-  )
-  
-  print(plot_a)
-  plot_b = plot_csd_facility_map_fn(
-    CSD = CSD2,
-    facility ,
-    fill_var ,
-    fill_var_name ,
-    csd_sf ,
-    facility_sf  ,
-    min_val, max_val, step,
-    label_var = T
-  )
-  
-  print(plot_b)
-  
-  # Combine the two plots using patchwork and add a shared title
-  combined_plot <- (
-    plot_a + plot_b +
-      plot_layout(ncol = 2, guides = "collect") &
-      theme(legend.position = "bottom")
-  ) +
-    plot_annotation(
-      title = glue::glue("Remoteness measurement: {CSD1} vs {CSD2}"),
-      theme = theme(plot.title = element_text(
-        size = 16,
-        face = "bold",
-        hjust = 0.5
-      ))
-    )
-  
-  # Display the combined plot
-  print(combined_plot)
-  
-  # Save the combined plot
-  ggsave(
-    use_network_path(
-      glue::glue(
-        "2024 SES Index/data/output/remoteness/two_csd_{CSD1}_{CSD2}_avg_address_dist_{facility}_by_da.png"
-      )
-    ),
-    plot = combined_plot,
-    width = 14,
-    height = 7,
-    dpi = 300
-  )
-}
 
 
 compare_two_csd_in_map(
