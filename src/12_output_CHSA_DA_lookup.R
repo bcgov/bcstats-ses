15d784656189ef44613b3747cb38ff85558694a9
Duan <Jon.Duan@gov.bc.ca>
2025-06-23 23:19:28 -0700

    decide to use Jonathan's approach to get the DA-CHSA crosswalk.

--- src/12_output_CHSA_DA_lookup.R
+++ src/12_output_CHSA_DA_lookup.R
@@ -17,7 +17,9 @@ library(lubridate)
 library(glue)
 library(dbplyr)
 library(datadictionary)
+source("./src/utils.R") # get the functions for plotting maps
 
+province = "British Columbia"
 # Load configuration using config package
 # This will automatically look for a file named config.yml in the current and parent directory
 config <- config::get()
@@ -25,24 +27,145 @@ config <- config::get()
 
 # Extract settings from config
 
-gcs_table <- config$tables$gcs
-chsa_table <- config$tables$chsa
+#Setting paths
+bc_stats_project_lan <- config$file_path$bc_stats_project
 
-# Establish database connection using config values
-tryCatch(
-  {
-    con <- dbConnect(
-      odbc(),
-      Driver = config$database$driver,
-      Server = config$database$server,
-      Database = config$database$database,
-      trusted_connection = config$database$trusted_connection
-    )
-    cat("Successfully connected to the database\n")
-  },
-  error = function(e) {
-    stop(glue("Failed to connect to database: {e$message}"))
-  }
+Path_conversion <- file.path(
+  bc_stats_project_lan,
+  "ConversionTables",
+  "TranslationMasterFiles",
+  "2025"
+)
+
+
+#Loading the conversion table
+
+#Conversion table
+
+conversion_table <- readxl::read_xls(file.path(
+  Path_conversion,
+  paste0(
+    "Translation_Master_File_",
+    "2021_Census_Completed_",
+    "Provincial_Runs_2025_01_10",
+    " (identifiers).xls"
+  )
+))
+
+# get population estiamte in DB level.
+conversion_table_db_chsa_pop <- conversion_table %>%
+  select(DBUID = "DISSEMINATION_BLOCK_ID", CHSA, Pop_2021_Adjusted) # we don't use the DA id from this table. Instead, we use the DA id from geo attr table from StatsCAN
+
+
+######################################################################################################
+# Geographic attribute file: "2021_92-151_X statcan" refers to the 2021 Geographic Attribute File from Statistics Canada. This file contains geographic data at the dissemination block level, including information like geographic codes, names, population and dwelling counts, land area, unique identifiers, and DGUIDs. It's a key resource for understanding the geographic framework used in the 2021 Census.
+# https://www150.statcan.gc.ca/n1/pub/92-151-g/92-151-g2021001-eng.htm
+# https://www150.statcan.gc.ca/n1/en/catalogue/92-151-X
+######################################################################################################
+
+# check if csv file already exists
+gaf_csv_folder = file.path(
+  bc_stats_project_lan,
+  "Household Projections",
+  "10_Annual Household Projections",
+  "hsh2022",
+  "data",
+  "Geographical Attributes",
+  "2021"
+)
+
+gaf_csv_file_name <- "2021_92-151_X.csv"
+
+# gaf_csv_file = file.path(CISR_csv_folder, gaf_csv_file_name)
+
+# Download CISR data
+gaf_data <- download_and_process_dataset(
+  url = "https://www12.statcan.gc.ca/census-recensement/2021/geo/aip-pia/attribute-attribs/files-fichiers/2021_92-151_X.zip",
+  csv_folder = gaf_csv_folder,
+  file_name = gaf_csv_file_name,
+  filter_pattern = "2021"
+)
+
+geo_attrs <- gaf_data %>%
+  filter(PRENAME_PRANOM == province) %>%
+  select(
+    # Rename columns to make them similar to 2016/2011 data
+    DBUID = DBUID_IDIDU,
+    DAUID = DAUID_ADIDU
+    # PRUID = PRUID_PRIDU,
+    # PRNAME = PRENAME_PRANOM,
+    # CDUID = CDUID_DRIDU,
+    # CDNAME = CDNAME_DRNOM,
+    # CDTYPE = CDTYPE_DRGENRE,
+    # CCSUID = CCSUID_SRUIDU,
+    # CCSNAME = CCSNAME_SRUNOM,
+    # CSDUID = CSDUID_SDRIDU,
+    # CSDNAME = CSDNAME_SDRNOM,
+    # CSDTYPE = CSDTYPE_SDRGENRE,
+    # ERUID = ERUID_REIDU,
+    # ERNAME = ERNAME_RENOM,
+    # SACCODE = SACCODE_CSSCODE,
+    # SACTYPE = SACTYPE_CSSGENRE,
+    # CMAUID = CMAUID_RMRIDU,
+    # CMAPUID = CMAPUID_RMRPIDU,
+    # CMANAME = CMANAME_RMRNOM,
+    # CMATYPE = CMATYPE_RMRGENRE,
+    # CTUID = CTUID_SRIDU,
+    # CTNAME = CTNAME_SRNOM,
+    # ADAUID = ADAUID_ADAIDU
+  ) %>%
+  unique()
+
+
+#Joining with the conversion table
+conversion_table <- conversion_table_db_chsa_pop %>%
+  left_join(
+    geo_attrs %>%
+      mutate(across(c(DBUID, DAUID), .fns = as.character)),
+    by = "DBUID"
+  )
+
+#Selecting CHSA table
+conv_table_CHSA <- conversion_table %>%
+  select(DBUID, DAUID, CHSA, Pop_2021_Adjusted)
+
+#Changing names to work under the loop.
+names(conv_table_CHSA) <- c("DAs", "CHSA", "Pop_2021_Adjusted")
+
+#Creating population adjusted weights to distribute CHSAs
+conv_table_CHSA <- conv_table_CHSA %>%
+  group_by(DAs) %>%
+  mutate(All_pop = sum(Pop_2021_Adjusted, na.rm = TRUE)) %>%
+  ungroup() %>%
+  group_by(DAs, CHSA) %>%
+  summarise(
+    prorate = sum(Pop_2021_Adjusted, na.rm = TRUE),
+    All_pop = mean(All_pop),
+    .groups = "drop"
+  ) %>%
+  ungroup() %>%
+  group_by(DAs) %>%
+  mutate(count = n()) %>%
+  ungroup() %>%
+  group_by(DAs, CHSA) %>%
+  mutate(
+    prorate = prorate / All_pop,
+    prorate = ifelse(is.finite(prorate), prorate, 1 / count)
+  ) %>%
+  select(-c(All_pop, count)) %>%
+  rename(`Dissemination Area` = DAs) %>%
+  arrange(`Dissemination Area`, CHSA)
+
+write_csv(
+  conv_table_CHSA,
+  file.path(
+    LAN,
+    "Requests",
+    "Ad hoc data requests",
+    "DA to CHSA conversion",
+    "output",
+    "Dissemination Area to CHSAs.csv"
+  )
 )
 
 
@@ -72,18 +195,21 @@ cat("Joining GCS data with CHSA data...\n")
 gcs_chsa_da_data <- gcs_data %>%
   left_join(chsa_data, by = "CHSA")
 
+nrow_gcs_chsa <- gcs_chsa_da_data %>% count() %>% collect() %>% pull()
+# Check if the join was successful
+if (nrow_gcs_chsa == 0) {
+  stop("Join failed: GCS data and CHSA data do not match.")
+} else {
+  cat(glue("Join successful: {nrow_gcs_chsa} rows returned.\n"))
+}
+
+
 gcs_chsa_da_cnt_data = gcs_chsa_da_data %>%
   group_by(CDCSD_2021, CHSA, MUN_NAME_2021, CHSA_NAME) %>%
   summarise(DA_CNT = n_distinct(DA)) %>%
   select(CDCSD_2021, CHSA, DA_CNT, MUN_NAME_2021, CHSA_NAME) %>% # have to move the character column to the end of the select due to MSSQL issue.
   collect()
 
-# Check if the join was successful
-if (nrow(gcs_chsa_da_data) == 0) {
-  stop("Join failed: GCS data and CHSA data do not match.")
-} else {
-  cat(glue("Join successful: {nrow(gcs_data)} rows returned.\n"))
-}
 
 # count how many distinct DAs are in the GCS data
 distinct_da_count <- gcs_data %>%
@@ -92,6 +218,14 @@ distinct_da_count <- gcs_data %>%
   length()
 
 cat(glue("Distinct DAs in GCS data: {distinct_da_count}\n"))
+
+# count how many distinct CHSAs are in the GCS data
+distinct_chsa_count <- gcs_data %>%
+  distinct(CHSA) %>%
+  pull(CHSA) %>%
+  length()
+
+cat(glue("Distinct CHSAs in GCS data: {distinct_chsa_count}\n"))
 
 # count how many distinct DAs are in the GCS data by CHSA
 distinct_da_chsa_count <- gcs_data %>%
@@ -132,15 +266,6 @@ if (nrow(da_multiple_chsa) > 0) {
 } else {
   cat("No DAs covering multiple CHSAs found.\n")
 }
-
-
-# count how many distinct CHSAs are in the GCS data
-distinct_chsa_count <- gcs_data %>%
-  distinct(CHSA) %>%
-  pull(CHSA) %>%
-  length()
-
-cat(glue("Distinct CHSAs in GCS data: {distinct_chsa_count}\n"))
 
 
 # Check if one CHSA is covering two CDCSD
