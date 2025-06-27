0d292d90cc5268021872aeaa5755d2f1931502cb
Duan <Jon.Duan@gov.bc.ca>
2025-06-26 18:17:24 -0700

    12.r uses GCS, and Jonathan's TMF to create DB-CHSA crosswalk, and then join to Jonathan's population table to create a DA-CHSA crosswalk with population as weight. 13.r extracte Jonathan's DA population table.

--- src/12_output_CHSA_DA_lookup.R
+++ src/12_output_CHSA_DA_lookup.R
@@ -26,7 +26,7 @@ config <- config::get()
 # use this config <- config::get(config = "development" ) to switch to development environment.
 
 # Extract settings from config
-
+gcs_table <- config$tables$gcs
 chsa_table <- config$tables$chsa
 
 # Establish database connection using config values
@@ -54,8 +54,94 @@ chsa_data <- tbl(con, in_schema("Prod", chsa_table)) %>%
 
 chsa_data %>% glimpse()
 
+#####################################################################
+# Create GCS data reference, use this table to create a crosswalk of CHSA and DB from 2016 to 2023
+# and then later join the DB population to create weights for crosswolk of CHSA and DA
+#####################################################################
+cat("Retrieving GCS data...\n")
 
-#Setting paths
+# postal code region level
+tbl_long_cols_mssql(con, "Prod", gcs_table) %>%
+  # tbl(con, in_schema("Prod", gcs_table)) %>%
+  filter(ACTIVE == 'Y') %>%
+  colnames() %>%
+  paste0(collapse = ",")
+glimpse()
+
+gcs_data <- tbl_long_cols_mssql(con, "Prod", gcs_table) %>% collect()
+
+gcs_data |>
+  count(DB_2021)
+# only 29,459 DBs??
+gcs_data |>
+  count(DA_2021)
+# only 3,714 DAs?? Not good, will consider to add Jonathan's TMF a
+
+# gcs_data %>%
+#   filter(
+#     DB_2021 == "59090896001"
+#   )
+#
+# gcs_data %>%
+#   filter(
+#     DB_2016 == "59090896001"
+#   )
+#
+# # DB 59090896001 was not in 2016, and was initiated in 2021
+# # and all those 7 postal code regions switched from one DB to another DB
+#
+# gcs_data %>%
+#   filter(
+#     DB_2021 == "59090124002"
+#   )
+#
+# gcs_data %>%
+#   filter(
+#     DB_2016 == "59090124002"
+#   )
+#
+# # DB 59090124002 was in 2016 and breaks into different DB in 2021
+# # DB only 2021 and 2016, but DA CSD back to 2011
+# it may cause problem later
+
+# start working on DB2021 and CHSA
+# each row/year is expanded to 2019:2023
+gcs_chsa_db_lookup_2021 <- gcs_data %>%
+  count(
+    CHSA,
+    DAUID = DA_2021,
+    DBUID = DB_2021,
+    name = "CNT_POSTALCODE"
+  ) %>%
+  tidyr::expand_grid(YEAR = 2019:2023) %>%
+  left_join(chsa_data)
+
+
+# start working on DB2021 and CHSA
+gcs_chsa_db_lookup_2016 <- gcs_data %>%
+  count(
+    CHSA,
+    DAUID = DA_2016,
+    DBUID = DB_2016,
+    name = "CNT_POSTALCODE"
+  ) %>%
+  tidyr::expand_grid(YEAR = 2016:2018) %>%
+  left_join(chsa_data)
+
+
+gcs_chsa_db_lookup_2016_2023 <- gcs_chsa_db_lookup_2021 %>%
+  bind_rows(gcs_chsa_db_lookup_2016) |>
+  select(-CNT_POSTALCODE) |>
+  select(YEAR, CHSA, CHSA_NAME, DBUID)
+
+gcs_chsa_db_lookup_2016_2023 |> glimpse()
+# 234,073
+#########################################################################
+# Use Jonathan's TMF file, limitation is that it only has DB 2021 and DA 2021.
+# So may take both and make a set of CHSA, DB, and YEAR
+#########################################################################
+
+#Setting pathsAdd commentMore actions
 bc_stats_project_lan <- config$file_path$bc_stats_project
 
 Path_conversion <- file.path(
@@ -68,7 +154,7 @@ Path_conversion <- file.path(
 
 #Loading the conversion table
 
-#Conversion table is a table where population team saves estimated population
+#Conversion table
 
 conversion_table <- readxl::read_xls(file.path(
   Path_conversion,
@@ -80,124 +166,94 @@ conversion_table <- readxl::read_xls(file.path(
   )
 ))
 
-# get population estimate in DB level. There are many geography in this table, but we don't use them.
-conversion_table_db_chsa_pop <- conversion_table %>%
-  select(DBUID = "DISSEMINATION_BLOCK_ID", CHSA, Pop_2021_Adjusted) # we don't use the DA id from this table. Instead, we use the DA id from geo attr table from StatsCAN according to Jonathan
+conversion_table |>
+  # glimpse()
+  count(DISSEMINATION_AREA_ID)
+# 7,848
 
-
-######################################################################################################
-# Geographic attribute file: "2021_92-151_X statcan" refers to the 2021 Geographic Attribute File from Statistics Canada. This file contains geographic data at the dissemination block level, including information like geographic codes, names, population and dwelling counts, land area, unique identifiers, and DGUIDs. It's a key resource for understanding the geographic framework used in the 2021 Census.
-# https://www150.statcan.gc.ca/n1/pub/92-151-g/92-151-g2021001-eng.htm
-# https://www150.statcan.gc.ca/n1/en/catalogue/92-151-X
-######################################################################################################
-
-# check if csv file already exists
-gaf_csv_folder = file.path(
-  bc_stats_project_lan,
-  "Household Projections",
-  "10_Annual Household Projections",
-  "hsh2022",
-  "data",
-  "Geographical Attributes",
-  "2021"
-)
-
-gaf_csv_file_name <- "2021_92-151_X.csv"
-
-# gaf_csv_file = file.path(CISR_csv_folder, gaf_csv_file_name)
-
-# Download and process CISR data
-gaf_data <- download_and_process_dataset(
-  url = "https://www12.statcan.gc.ca/census-recensement/2021/geo/aip-pia/attribute-attribs/files-fichiers/2021_92-151_X.zip",
-  csv_folder = gaf_csv_folder,
-  file_name = gaf_csv_file_name,
-  filter_pattern = "2021"
-)
-
-geo_attrs <- gaf_data %>%
-  filter(PRENAME_PRANOM == province) %>%
+db_da_chsa_2021_jonathan <- conversion_table |>
   select(
-    # Rename columns to make them similar to 2016/2011 data
-    DBUID = DBUID_IDIDU,
-    DAUID = DAUID_ADIDU
-    # PRUID = PRUID_PRIDU,# for the future use.
-    # PRNAME = PRENAME_PRANOM,
-    # CDUID = CDUID_DRIDU,
-    # CDNAME = CDNAME_DRNOM,
-    # CDTYPE = CDTYPE_DRGENRE,
-    # CCSUID = CCSUID_SRUIDU,
-    # CCSNAME = CCSNAME_SRUNOM,
-    # CSDUID = CSDUID_SDRIDU,
-    # CSDNAME = CSDNAME_SDRNOM,
-    # CSDTYPE = CSDTYPE_SDRGENRE,
-    # ERUID = ERUID_REIDU,
-    # ERNAME = ERNAME_RENOM,
-    # SACCODE = SACCODE_CSSCODE,
-    # SACTYPE = SACTYPE_CSSGENRE,
-    # CMAUID = CMAUID_RMRIDU,
-    # CMAPUID = CMAPUID_RMRPIDU,
-    # CMANAME = CMANAME_RMRNOM,
-    # CMATYPE = CMATYPE_RMRGENRE,
-    # CTUID = CTUID_SRIDU,
-    # CTNAME = CTNAME_SRNOM,
-    # ADAUID = ADAUID_ADAIDU
+    CHSA,
+    DBUID = "DISSEMINATION_BLOCK_ID",
+    DAUID = "DISSEMINATION_AREA_ID"
+  ) |>
+  distinct() |>
+  mutate(CHSA = as.character(CHSA)) |>
+  left_join(chsa_data)
+
+db_da_chsa_2021_jonathan |> glimpse()
+# 52423 DBs
+
+db_da_chsa_2016_2023_jonathan <- db_da_chsa_2021_jonathan |>
+  expand_grid(YEAR = 2016:2023) |>
+  select(YEAR, CHSA, CHSA_NAME, DBUID)
+
+db_da_chsa_2016_2023_jonathan |> glimpse()
+# 419,384 rows
+
+db_da_chsa_lookup_2016_2023 <- gcs_chsa_db_lookup_2016_2023 |>
+  bind_rows(db_da_chsa_2016_2023_jonathan) |>
+  distinct()
+
+db_da_chsa_lookup_2016_2023 |>
+  glimpse()
+# 429,323 rows
+# So we recover some (10000) DB2016 from GCS table
+
+#################################################################
+# DB population from Jonathan
+#################################################################
+#Setting paths
+bc_stats_project_lan <- config$file_path$bc_stats_project
+
+# replace this data source with Jonathan's DB population estimate from 2016 to 2023
+
+# file path of DB population estimates
+db_population_file_path = file.path(
+  bc_stats_project_lan,
+  config$file_path$population_folder
+)
+
+# list all the rds files
+db_population_files = list.files(db_population_file_path, full.names = T)
+# read all the rds files
+db_population_df = db_population_files %>%
+  # Filter to include only RDS files and exclude directories
+  purrr::keep(function(path) {
+    file.exists(path) &&
+      !dir.exists(path) &&
+      tolower(tools::file_ext(path)) == "rds"
+  }) %>%
+  # Read the remaining RDS files
+  purrr::map(readRDS) %>%
+  bind_rows()
+
+
+# get population estimate in DB level. There are many geography in this table, but we don't use them.
+db_population_after2016_df <- db_population_df %>%
+  filter(Sex == "Total") %>%
+  filter(Age == "TOTAL") %>%
+  select(-Sex, -Age) %>%
+  filter(Type == "Dissemination Block") %>%
+  select(-Type) %>%
+  select(
+    YEAR = Year,
+    DBUID = ID,
+    DAUID = parentID,
+    POPULATION = Pop_2021_Adjusted
   ) %>%
-  unique()
-
-
-#Joining with the conversion table
-conversion_table_db_da_chsa_pop <- conversion_table_db_chsa_pop %>%
-  left_join(
-    geo_attrs %>%
-      mutate(across(c(DBUID, DAUID), .fns = as.character)),
-    by = "DBUID"
+  filter(
+    YEAR >= 2016
   )
 
+db_population_after2016_df |> glimpse()
 
-# count how many distinct DBUIDs are in the GCS data
-distinct_db_count <- conversion_table_db_da_chsa_pop %>%
-  distinct(DBUID) %>%
-  pull(DBUID) %>%
-  length()
-
-cat(glue("Distinct DBUIDs in GCS data: {distinct_db_count}\n"))
-
-# count how many distinct DAUIDs are in the GCS data
-distinct_da_count <- conversion_table_db_da_chsa_pop %>%
-  distinct(DAUID) %>%
-  pull(DAUID) %>%
-  length()
-
-cat(glue("Distinct DBUIDs in GCS data: {distinct_da_count}\n"))
-
-# count how many distinct CHSAs are in the GCS data
-distinct_chsa_count <- conversion_table_db_da_chsa_pop %>%
-  distinct(CHSA) %>%
-  pull(CHSA) %>%
-  length()
-
-cat(glue("Distinct CHSAs in GCS data: {distinct_chsa_count}\n"))
-
-# Check if one DB is covering two CHSAs
-db_multiple_chsa <- conversion_table_db_da_chsa_pop %>%
-  group_by(DBUID) %>%
-  summarise(CHSA_CNT = n_distinct(CHSA)) %>%
-  filter(CHSA_CNT > 1) %>%
-  collect()
-
-if (nrow(db_multiple_chsa) > 0) {
-  cat(glue(
-    "WARNING: {nrow(db_multiple_chsa)} DBs are covering multiple CHSAs!\n"
-  ))
-  print(db_multiple_chsa)
-} else {
-  cat("No DBs covering multiple CHSAs found.\n")
-}
-
-# good, no DB has covered two CHSAs.
+db_population_after2016_df |>
+  count(DAUID)
+# 7,848 DAs.
 
 # Check if one DB is covering two DAs
-db_multiple_da <- conversion_table_db_da_chsa_pop %>%
+db_multiple_da <- db_population_after2016_df %>%
   group_by(DBUID) %>%
   summarise(DA_CNT = n_distinct(DAUID)) %>%
   filter(DA_CNT > 1) %>%
@@ -212,24 +268,125 @@ if (nrow(db_multiple_da) > 0) {
   cat("No DBs covering multiple DAs found.\n")
 }
 
-# good, no DB has covered two DAs.
+#########################################################################
+# Join two tables together and gcs/TMF as base
+#########################################################################
+
+bc_db_da_chsa_pop <- db_da_chsa_lookup_2016_2023 |>
+  left_join(
+    db_population_after2016_df |>
+      mutate(DBUID = as.character(DBUID)),
+    by = join_by(YEAR, DBUID)
+  )
+
+bc_db_da_chsa_pop |> glimpse()
+# Rows: 429,323
+
+# fix NA DAUID, if DAUIA from population estimates table is missing, we extract them from DBUID
+bc_db_da_chsa_pop |>
+  filter(is.na(DAUID))
+
+bc_db_da_chsa_pop <- bc_db_da_chsa_pop |>
+  mutate(
+    DAUID = as.character(DAUID),
+    DAUID = if_else(
+      is.na(DAUID),
+      str_sub(DBUID, 1, 8),
+      DAUID
+    )
+  )
+
+##########################################################
+# Validation
+#########################################################
+
+# count how many distinct DBUIDs are in the GCS data
+distinct_db_count <- bc_db_da_chsa_pop %>%
+  distinct(DBUID) %>%
+  pull(DBUID) %>%
+  length()
+
+cat(glue("Distinct DBUIDs in GCS data: {distinct_db_count}\n"))
+
+# count how many distinct DAUIDs are in the GCS data
+distinct_da_count <- bc_db_da_chsa_pop %>%
+  distinct(DAUID) %>%
+  pull(DAUID) %>%
+  length()
+
+cat(glue("Distinct DBUIDs in GCS data: {distinct_da_count}\n"))
+
+# count how many distinct CHSAs are in the GCS data
+distinct_chsa_count <- bc_db_da_chsa_pop %>%
+  distinct(CHSA) %>%
+  pull(CHSA) %>%
+  length()
+
+cat(glue("Distinct CHSAs in GCS data: {distinct_chsa_count}\n"))
+
+# Check if one DB is covering two CHSAs
+db_multiple_chsa <- bc_db_da_chsa_pop %>%
+  group_by(DBUID, YEAR) %>%
+  summarise(CHSA_CNT = n_distinct(CHSA)) %>%
+  filter(CHSA_CNT > 1) %>%
+  collect()
+
+if (nrow(db_multiple_chsa) > 0) {
+  cat(glue(
+    "WARNING: {nrow(db_multiple_chsa)} DBs are covering multiple CHSAs!\n"
+  ))
+  print(db_multiple_chsa)
+} else {
+  cat("No DBs covering multiple CHSAs found.\n")
+}
+
+# 4 DBs covered two CHSAs.
+# for those four DB, their population should be shared by tow CHSAs
+# so assume 50% share for each CHSA
+
+bc_db_da_chsa_pop <- bc_db_da_chsa_pop |>
+  mutate(
+    POPULATION = if_else(
+      DBUID %in% (db_multiple_chsa |> distinct(DBUID) |> pull(DBUID)),
+      POPULATION / 2,
+      POPULATION
+    )
+  )
+
+# Check if one DB is covering two DAs
+db_multiple_da <- bc_db_da_chsa_pop %>%
+  group_by(DBUID, YEAR) %>%
+  summarise(DA_CNT = n_distinct(DAUID)) %>%
+  filter(DA_CNT > 1) %>%
+  collect()
+
+if (nrow(db_multiple_da) > 0) {
+  cat(glue(
+    "WARNING: {nrow(db_multiple_da)} DBs are covering multiple DAs!\n"
+  ))
+  print(db_multiple_da)
+} else {
+  cat("No DBs covering multiple DAs found.\n")
+}
+
+# No DBs have covered two DAs.
 
 # Check if one DA is covering two CHSAs
-da_multiple_chsa <- conversion_table_db_da_chsa_pop %>%
-  group_by(DAUID) %>%
+da_multiple_chsa <- bc_db_da_chsa_pop %>%
+  group_by(DAUID, YEAR) %>%
   summarise(CHSA_CNT = n_distinct(CHSA)) %>%
   filter(CHSA_CNT > 1) %>%
   collect()
 
 if (nrow(da_multiple_chsa) > 0) {
   cat(glue(
-    "WARNING: {nrow(da_multiple_chsa)} DAs are covering multiple CHSAs!\n"
+    "WARNING: {nrow(da_multiple_chsa)} DAs and YEAR combination are covering multiple CHSAs!\n"
   ))
   print(da_multiple_chsa)
 
   # If you want to see the details of these DAs with their multiple CHSAs
   # problem_das <- da_multiple_chsa %>% pull(DAUID)
-  # da_chsa_details <- conversion_table_db_da_chsa_pop %>%
+  # da_chsa_details <- bc_db_da_chsa_pop %>%
   #   filter(DAUID %in% problem_das) %>%
   #   count(DAUID, CHSA) %>%
   #   collect()
@@ -240,7 +397,6 @@ if (nrow(da_multiple_chsa) > 0) {
 }
 
 
-# no duplication or overlapping if group by DB
 # so if our base table is DA level, and we want to aggregate DA level table to CHSA level table,
 # we need to split DAs into different CHSAs with population as weights.
 # for example, 20% DA01's population contribute to CHSA01, and 80% DA01's population contribute to CHSA02.
@@ -251,24 +407,24 @@ if (nrow(da_multiple_chsa) > 0) {
 # we call the new intermediate region as CHSADA which are smaller than CHSA and DA but larger than DB.
 
 # Now we have a DB level table
-#Creating population adjusted weights to distribute CHSAs
-db_da_chsa_pop_cnt <- conversion_table_db_da_chsa_pop %>%
-  group_by(CHSA, DAUID) %>%
+# Creating population adjusted weights to distribute CHSAs
+db_to_da_chsa_pop_cnt <- bc_db_da_chsa_pop %>%
+  group_by(YEAR, CHSA, DAUID) %>%
   mutate(
-    chsada_pop = sum(Pop_2021_Adjusted, na.rm = TRUE),
+    chsada_pop = sum(POPULATION, na.rm = TRUE),
     cnt_db_in_chsada = n()
   ) %>%
   ungroup() %>%
-  group_by(DAUID) %>%
+  group_by(YEAR, DAUID) %>%
   mutate(
-    da_pop = sum(Pop_2021_Adjusted, na.rm = TRUE),
+    da_pop = sum(POPULATION, na.rm = TRUE),
     chsada_to_da_pop_ratio = chsada_pop / da_pop,
     cnt_db_in_da = n() # the same within da
   ) %>%
   ungroup() %>%
-  group_by(CHSA) %>%
+  group_by(YEAR, CHSA) %>%
   mutate(
-    chsa_pop = sum(Pop_2021_Adjusted, na.rm = TRUE),
+    chsa_pop = sum(POPULATION, na.rm = TRUE),
     chsada_to_chsa_pop_ratio = chsada_pop / chsa_pop,
     cnt_db_in_chsa = n() # the same within chsa
   ) %>%
@@ -277,6 +433,7 @@ db_da_chsa_pop_cnt <- conversion_table_db_da_chsa_pop %>%
   # the weights for our purpose (aggregate da value to chsa value) will be chsada_pop/chsa_pop for da value.
   # the weights for disaggregate chsa value to da value) will be chsada_pop/da_pop for da value, which is the weight/prorate in Jonathan's original code.
   count(
+    YEAR,
     chsada_pop,
     cnt_db_in_chsada,
     DAUID,
@@ -291,15 +448,16 @@ db_da_chsa_pop_cnt <- conversion_table_db_da_chsa_pop %>%
     name = "cnt_db" # should be the same as cnt_db_in_chsada, remove it later
   ) %>%
   mutate(
-    chsada_id = row_number(),
+    chsada_id = str_c(CHSA, DAUID), # Not work if we have year
     CHSA = as.character(CHSA)
-  ) %>%
-  left_join(chsa_data)
-db_da_chsa_pop_cnt %>% glimpse()
+  )
+
+
+db_to_da_chsa_pop_cnt %>% glimpse()
 # the number of chsada is similar to the number of DA. 8084
-cat(glue::glue("There are {nrow(db_da_chsa_pop_cnt)} CHSADAs found.\n"))
+cat(glue::glue("There are {nrow(db_to_da_chsa_pop_cnt)} CHSADAs found.\n"))
 # original from Jonathan
-# conv_table_CHSA <- conversion_table_db_da_chsa_pop %>%
+# conv_table_CHSA <- bc_db_da_chsa_pop %>%
 #   group_by(DAs) %>%
 #   mutate(All_pop = sum(Pop_2021_Adjusted, na.rm = TRUE)) %>% # All_pop is the DA population
 #   ungroup() %>%
@@ -322,13 +480,13 @@ cat(glue::glue("There are {nrow(db_da_chsa_pop_cnt)} CHSADAs found.\n"))
 #   rename(`Dissemination Area` = DAs) %>%
 #   arrange(`Dissemination Area`, CHSA)
 
-db_da_chsa_pop_cnt %>%
+db_to_da_chsa_pop_cnt %>%
   glimpse()
 
 write_csv(
-  db_da_chsa_pop_cnt %>%
+  db_to_da_chsa_pop_cnt %>%
     select(-cnt_db),
-  here::here("out", "db_da_chsa_pop_cnt.csv")
+  here::here("out", "db_to_da_chsa_pop_cnt.csv")
 )
 
 db_da_chsa_pop_csv_folder = file.path(
@@ -339,7 +497,7 @@ db_da_chsa_pop_csv_folder = file.path(
 write_csv(
   db_da_chsa_pop_cnt %>%
     select(-cnt_db),
-  file.path(db_da_chsa_pop_csv_folder, "db_da_chsa_pop_cnt")
+  file.path(db_da_chsa_pop_csv_folder, "db_to_da_chsa_pop_cnt.csv")
 )
 
 
